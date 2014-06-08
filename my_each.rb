1cfe6c3935dd1527649819ad6f0f837bac06817c
ahimmelstoss <ahimmelstoss@gmail.com>
2014-06-08 12:03:40 -0400

    updated rspec 3 and fixed empty collection spec

--- my_each.rb
+++ my_each.rb
@@ -4,6 +4,5 @@ def my_each(collection)
     yield(collection[i])
     i = i + 1
   end
-
   collection
 end
--- spec/my_each_spec.rb
+++ spec/my_each_spec.rb
@@ -5,7 +5,7 @@ describe "my_each" do
     words = ['hi', 'hello', 'bye', 'goodbye']
 
     # This line tests that each word gets printed out by the loop!
-    $stdout.should_receive(:puts).exactly(words.length)
+    expect($stdout).to receive(:puts).exactly(words.length).times
 
     my_each(words) do |word|
       puts word
@@ -15,16 +15,20 @@ describe "my_each" do
   it "can handle an empty collection" do
     empty_array = []
 
-    my_each(empty_array) do
-      raise 'This block shouldnt run for empty arrays!'
-    end.should_not raise_error
+    saved_block = Proc.new {
+      my_each(empty_array) do |x|
+        raise "This block should not run!"
+      end
+    }
+
+    expect(saved_block).to_not raise_error
   end
 
   it "returns the original collection" do
     tas = ['arel', 'spencer', 'jon', 'logan']
 
-    my_each(tas) do |ta|
+    expect(my_each(tas) do |ta|
       puts "#{ta} is awesome"
-    end.should == tas
+    end).to eq(tas)
   end
 end
