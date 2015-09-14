9cf0b5c0a0d911e5524af2271f6730705a8008a5
deniznida <denizunat@hotmail.com>
2015-09-14 13:44:28 -0400

    add new spec thats test if block is run count times and single element is passed into block

--- spec/my_each_spec.rb
+++ spec/my_each_spec.rb
@@ -38,23 +38,6 @@ describe "my_each" do
   end
 
 
-  it "uses yield" do
-    tas = ['arel', 'spencer', 'jon', 'logan']
-    # array may be modified by the iteration function so 
-    # we cannot use it for verifying the results
-    # therefore we create a new copy using the clone method 
-    tas_original = tas.clone
-
-    yield_count = 0
-
-    my_each(tas) do |ta|
-      yield_count +=1
-    end
-
-    expect(yield_count).to eq(tas_original.length)
-  end
-
-
   it "yields the correct element" do
     words = ['hi', 'hello', 'bye', 'goodbye']
     my_each(words) do |word|
@@ -97,4 +80,30 @@ describe "my_each" do
     
     end
 
+  it "test that the block is run n times" do
+    tas = ['arel', 'jon', 'logan', 'spencer']
+    expected = tas.length
+    times_called = 0
+
+    my_each(tas) do |ta|
+      times_called += 1
+    end
+
+    expect(times_called).to eq(expected)
+    
+  end
+
+  it "that only a single element is passed into that block." do
+
+    tas = ['arel', 'jon', 'logan', 'spencer']
+    expected = tas.length
+    times_called = 0
+
+    my_each(tas) do |ta|
+      # cannot be an array
+      expect(ta.kind_of?(Array)).to eq(false)
+      expect(ta.kind_of?(String)).to eq(true)
+    end
+
+  end
 end
