1ed94d57d990cc37d65c9eecc8751f46399b6c7b
msuzoagu <msuzoagu@gmail.com>
2015-12-22 14:58:33 -0500

    fix confusion about inclusion of while keyword

--- spec/my_each_spec.rb
+++ spec/my_each_spec.rb
@@ -6,18 +6,20 @@ describe "my_each" do
     words = ['hi', 'hello', 'bye', 'goodbye']
 
     expect(words).to_not receive(:each)
-    
-    my_each(words) do |word|
-      # Do nothing on yield
-    end
+
   end
 
-  it "does not call on while or puts" do 
+  it "does not call on puts" do
     file = File.read('./my_each.rb')
     contents = file.split(" ")
-    expect(contents).to_not include("while" || "puts")
+    expect(contents).to_not include("puts")
   end
 
+  it "calls on while" do
+    file = File.read('./my_each.rb')
+    contents = file.split(" ")
+    expect(contents).to include("while")
+  end
 
   it "iterates over each element" do
     words = ['hi', 'hello', 'bye', 'goodbye']
@@ -29,6 +31,15 @@ describe "my_each" do
     end
   end
 
+  it 'yields the correct element' do
+    words = ['hi', 'hello', 'bye', 'goodbye']
+    counter = 0
+
+    my_each(words) do |item|
+      expect(item).to equal(words[counter])
+      counter+=1
+    end
+  end
 
   it "can handle an empty collection" do
     empty_array = []
@@ -42,17 +53,9 @@ describe "my_each" do
     expect(saved_block).to_not raise_error
   end
 
-
-  it "yields the correct element" do
-    words = ['hi', 'hello', 'bye', 'goodbye']
-    my_each(words) do |word|
-      expect(word).to be_a(String)
-    end
-  end
-
   it "returned array contains the same elements as the original collection" do
     tas = ['arel', 'jon', 'logan', 'spencer']
-    # array may be modified by the iteration function so 
+    # array may be modified by the iteration function so
     # we cannot use it for verifying the results
     # therefore we create a new copy using the clone method
     tas_original = tas.clone
@@ -62,13 +65,12 @@ describe "my_each" do
     expect(my_each(tas) do |ta|
       # Do nothing on yield
     end).to contain_exactly('arel', 'jon', 'logan', 'spencer')
-  
-  end
 
+  end
 
   it "does not modify the original collection" do
     tas = ['arel', 'jon', 'logan', 'spencer']
-    # array may be modified by the iteration function so 
+    # array may be modified by the iteration function so
     # we cannot use it for verifying the results
     # therefore we create a new copy using the clone method
     tas_original = tas.clone
@@ -78,10 +80,10 @@ describe "my_each" do
       # Do nothing on yield
     end
 
-    # is verifying if the array we passed to method  
-    # has not been modified 
+    # is verifying if the array we passed to method
+    # has not been modified
     expect(tas).to eq(tas_original)
-    
+
     end
 
   it "block is run n times" do
@@ -94,7 +96,7 @@ describe "my_each" do
     end
 
     expect(times_called).to eq(expected)
-    
+
   end
 
   it "only single element is passed into block" do
