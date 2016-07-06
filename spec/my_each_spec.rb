a6651d8943f9e39f9642a26f1e09b36afdc3ef90
ga-be <ga-be@users.noreply.github.com>
2016-07-06 01:52:40 -0400

    more elegant solution

--- spec/my_each_spec.rb
+++ spec/my_each_spec.rb
@@ -3,18 +3,17 @@ require 'pry'
 
 describe "my_each" do
   file = File.read('./my_each.rb')
-  contents = file.split(%r{\s|\.}).uniq
   
-  it "does not call on each" do
-    expect(contents).to_not include("each")
+  it "does not call on .each" do
+    expect(file).to_not include(".each")
   end
 
   it "does not call on puts" do
-    expect(contents).to_not include("puts")
+    expect(file).to_not include("puts")
   end
 
   it "calls on while" do
-    expect(contents).to include("while")
+    expect(file).to include("while")
   end
 
   it "iterates over each element" do
