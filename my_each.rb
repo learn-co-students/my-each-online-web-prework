aea3abfa61da2f21409bf4a64da7798ab4a949a9
Carolyn Whelpley <carolynwhelpley@Jeffs-MacBook-Pro-3.local>
2018-09-13 10:41:22 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each (array)
+  if array.length == 0 
+    false
+  end
+  index = 0
+  while index < array.length
+    
+    yield(array[index])
+    index = index + 1
+    end
+  array
 end
