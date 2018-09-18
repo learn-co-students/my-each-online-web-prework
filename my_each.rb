1e5b9c02ca455012abd3193b8c50009812c301d6
Manny Pissanos <mpissano@gmail.com>
2018-09-18 20:52:49 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(arr)
+  if block_given?
+    i = 0
+    
+    while i < arr.length
+    yield (arr[i])
+  end
+ arr
+end
+
+my_each(arr) do |i|
+  puts i
 end
