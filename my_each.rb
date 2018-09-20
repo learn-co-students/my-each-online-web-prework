1c4c946edcc1be2b63a96c39e19cfad7c3fec328
Harry Bloch <harryfbloch@gmail.com>
2018-09-20 17:41:43 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  if !array.empty?
+    i = 0
+    
+    while i < array.length
+    yield(array[i])
+    i += 1
+    end
+  else
+    #empty array
+  end
+  array
 end
+
