9b40254c0a5f2cd1d5d026de9d4b3f3ccbd59be9
Kevin J Gomez <kjgmez1@gmail.com>
2018-09-26 21:54:50 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,9 @@
 def my_each (ar)
   counter = ar.length
   while counter > 0
-    yield (ar[counter])
-    ar
-    counter -= 1
+    yield do
+      ar[counter]
+      counter -= 1
+    end
   end
 end
