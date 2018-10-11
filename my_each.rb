6711df4f56a092965bcf70c242ea4d221b33e659
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-11 17:03:15 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
+def my_each(arr) # put argument(s) here
   # code here
+  i = 0 
+  while i < arr.length
+    yield(arr[i])
+    i += 1
+  end 
+    return arr
 end
