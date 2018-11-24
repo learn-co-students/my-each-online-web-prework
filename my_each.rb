3e1b45c134d676939a1ccd35609910fcf2fffc50
Whitny Edwards <wne4e2@gmail.com>
2018-11-24 23:44:36 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -2,8 +2,8 @@ def my_each(array) # put argument(s) here
   # code here
   i = 0
   while i < array.size
-    block_given? # optional
-    yield(array[i]) # if block, then yield to block # also need to pass argument(s) to yield, otherwise block is operating on nil
+    block_given? 
+    yield(array[i]) 
     i += 1
   end
 array
