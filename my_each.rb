2f446daf53902e5172f197bd9638d749c28f4503
michael hussain <smhussain_1@yahoo.com>
2019-01-02 02:46:08 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,40 @@
-def my_each # put argument(s) here
-  # code here
+
+# def my_each(arr)
+#   counter = 0
+#   x = Array.new
+#   while counter < arr.length
+#     yield(arr[counter])
+#     x << arr[counter]
+#     counter += 1
+#   end
+#   return x
+# end
+
+
+
+
+def my_each(arr)
+  array = []
+  n = 0
+  while n < arr.length
+    yield(arr[n])
+    array << arr[n]
+    n += 1
+  end
+  array
 end
+
+# def my_each(array, n)
+#   array = []
+#   n = 1 
+#   while array.count < 8
+#   array.push(n)
+#   yield (array)
+#   n += 1
+#   end 
+# end
+
+
+# my_each(array, n) do |array| array*2 end 
+
+
