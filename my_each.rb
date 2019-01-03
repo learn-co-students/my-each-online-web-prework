276dba7054f88c5ccbb6d653571ad7f38b405786
Max Brooks <maxbrooks114@gmail.com>
2019-01-03 20:32:25 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each (array)
+if array.length==0 
+  "Wowzers!"
+ else 
+i = 0
+ while i < 4
+    yield array[i]
+    i += 1
+  end
+  array
+end
 end
