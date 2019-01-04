60c3b3a76fe8f9d09717fc839dd192d159b9ee6f
A Glazer <avigayilglazer@gmail.com>
2019-01-04 10:17:58 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,13 @@
-def my_each(num)
-  num = 0
-  while num < 5
-    yield(num)
-    num += 1
+words = ['hi', 'hello', 'bye', 'goodbye']
+
+def my_each(words)
+  x = 0
+  while x < words.length
+    yield words[x]
+    x = x + 1
   end
 end
 
-my_each(num) {|x| "The number is #{x}."}
+my_each(words) do |x|
+  "The number of letters is #{x}."
+end
