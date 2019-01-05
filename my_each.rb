40a6aa226c288c182d1f3aed12662b0aa3265ba0
Luke Granger <grangerl330@gmail.com>
2019-01-05 17:24:46 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,8 @@
 def my_each(collection)
-  if block_given?
     i = 0 
     while i < collection.length 
       yield collection[i]
       i = i + 1 
     end 
     collection 
-  else 
-    "No block given"
-  end 
 end
