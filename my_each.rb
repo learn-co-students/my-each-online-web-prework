b3954baa8e66e53ff457b0add70ba2deb3a1ce58
Trent Moss <trentmoss05@gmail.com>
2019-01-07 15:18:35 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,2 +1,9 @@
-def my_each(array) # put argument(s) here
+def my_each(collection) # put argument(s) here
   # code here
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i = i + 1
+  end
+  collection
+end
