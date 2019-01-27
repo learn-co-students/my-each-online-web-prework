27ed72f3e7da67ac98933bac5fceeacc6bef24ca
Lu Daley <daleyluis@gmail.com>
2019-01-27 17:08:45 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,18 +1,13 @@
 def my_each(words)
-   words = ['hi', 'hello', 'bye', 'goodbye']
-   if block_given?
-   my_each(words).map do |word|
-   yield("#{word}")
-   end 
-   #end
-my_each(word) {i}
-end 
+   counter = 0
+   while counter < words.length
+      yield(words[counter])
+      counter += 1
+   end
+   words.each {|n| puts n}
+   words
 end
-
 =begin
-
 You'll be building out the content of the #my_each method. This method should accept an argument of an array and use the while loop to iterate over each member of that array, yielding each element contained in the array to a block.
-
 Here's an example of what should happen when you call your my_each method:
-
 =end
