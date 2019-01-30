19e0614c9115ea14f0340340cda921dcba951990
Nataliya Rudenko <n.rudenko@gmail.com>
2019-01-30 20:17:46 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
-  # code here
+def my_each (counter)
+  while counter < 3
+  "The current number"
+  counter += 1
 end
+end
+
+def my_each(words)
+  i = 0 
+  while i < words.length
+  yield words[i]
+  i = i + 1 
+end 
+ words
+end
+  
+  
