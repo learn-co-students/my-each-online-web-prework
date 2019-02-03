5e00ae2096e7b760addb3c144fc6a7989eb0f438
Ming Duan <11203duan@gmail.com>
2019-02-03 23:18:42 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(a)
+  if block_given?
+  i = 0
+  while i < a.length
+    yield(a[i])
+    i += 1
+  end
+else "nothing"
+end
+a
+end
+
+
+my_each("a") do
+  |x|  "x"
 end
