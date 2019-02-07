1c5a36b2dfe3fe87e658af9024d687e9ed2e9c1b
Junyoung McArdle <juny.mcardle@gmail.com>
2019-02-07 20:41:09 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+  i = 0
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
+  array# code here
 end
