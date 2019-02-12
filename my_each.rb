0094d47a51c4c37d9c70a12b5dccd01cc774b8e2
Ryan Sise <kerneltux0@gmail.com>
2019-02-12 18:40:06 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  x = 0
+  new_array = []
+  while x < array.size
+    yield array[x]
+    new_array << array[x]
+    x = x+1
+  end
+  new_array
 end
