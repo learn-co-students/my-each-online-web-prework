cccec83da169f3e3ce6c273dfd700e08dfdee703
Ryan Pethel <arpethel@gmail.com>
2019-02-18 22:45:49 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i = 0
+
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+  array
+end
+
+my_each(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    "Hi, #{name}"
+  end
 end
