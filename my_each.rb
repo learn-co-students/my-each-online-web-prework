b2d4be63989db7c0c1f3339f3ad3b176a05f696d
David Stewart <davidstewart@gmail.com>
2019-02-27 16:27:35 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,30 @@
-def my_each # put argument(s) here
-  # code here
-end
+test = [1,2,3,4]
+require 'pry'
+def my_each(array)
+  i = 0
+   while i < array.length
+     yield array[i]
+      i = i + 1
+     end
+     return array
+ end
+
+
+
+
+
+
+# if block_given? 
+#   i = 0
+#   while array.length < i
+#   yield array[i]
+#   i = i + 1
+# end
+# else
+#   return array
+# end
+  
+# end
+
+#binding.pry
+
