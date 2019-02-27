545cd7acbb87198801bf4d8987267f4b662ec6e1
Charlotte Neff <charlotte.neff@gmail.com>
2019-02-27 02:00:53 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each(array) # put argument(s) here
-  while # code here
+def my_each(collection) # put argument(s) here
+  i = 0
+  while i < collection.length
+    yield (collection [i])
+    i = i + 1
+  end
+  collection
+  # code here
 end
