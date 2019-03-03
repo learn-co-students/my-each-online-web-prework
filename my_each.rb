4b06792fbf7fa15f8adebfa464644c6feb9149ec
Leeya Davis <leeya463@gmail.com>
2019-03-03 00:11:56 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+    if array != []
+      e = 0
+
+      while e < array.length #iterate over each element in array w/o using #each
+        yield array[e]
+        e = e + 1
+      end #use while loop and yeild each element contained in array to block
+    else
+      "This block should not run!"
+    end
+    array
 end
