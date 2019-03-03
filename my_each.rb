8d7ad1c83a057b900720ca039947b03e4d127ab4
Leeya <leeya463@gmail.com>
2019-03-03 13:01:19 -0500

    Update my_each.rb

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,9 @@
 def my_each(array)
-    if array != []
-      e = 0
+    e = 0
 
-      while e < array.length #iterate over each element in array w/o using #each
-        yield array[e]
-        e = e + 1
-      end #use while loop and yeild each element contained in array to block
-    else
-      "This block should not run!"
-    end
+    while e < array.length #iterate over each element in array w/o using #each
+      yield array[e]
+      e = e + 1
+    end #use while loop and yeild each element contained in array to block
     array
 end
