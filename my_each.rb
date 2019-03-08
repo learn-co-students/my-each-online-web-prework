85f3d32a38bde2f510b1c0c33d19110659023b2c
victorious-messaging-4309 <wildayk@gmail.com>
2019-03-08 16:41:20 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+
+collection = [1, 2, 3, 4]
+
+def my_each(collection)
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i = i + 1
+  end
+  collection
 end
