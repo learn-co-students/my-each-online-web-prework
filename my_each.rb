468bf39036e6113237abe5597c1dcdd11676f3c2
Andrew Waller <andrewhwaller@gmail.com>
2019-03-17 22:20:37 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  counter = 0
+  while counter < array.length
+    yield(array[counter])
+    counter = counter + 1
+  end
+  array
 end
