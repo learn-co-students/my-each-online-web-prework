52b74864b6eead921246e071a78365d7ee8b6b49
Cody Frank <cody.frank30@gmail.com>
2019-03-24 23:24:49 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  if block_given?
+    index = 0
+    while index < array.size
+      yield(array[index])
+      index += 1
+    end
+    array
+  end
 end
