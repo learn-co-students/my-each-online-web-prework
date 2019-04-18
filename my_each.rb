569b19c9fe8e5c391a6afecd9b01f5e8ae81c5b6
Jessica Hong <jessicahong.24@gmail.com>
2019-04-18 18:53:53 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+def my_each (words)
+  
+    i = 0
+    
+    while i < words.length 
+    yield (words [i])
+    i +=1
+  
+    i
+  
+  end
+  words
 end
