8ca9ba1a96d8d104a50841cf53ad6b01877f4a84
compassionate-scope-9333 <lexus2424@gmail.com>
2019-04-24 23:01:14 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  if block_given?
+    i = 0
+    
+    while i < array.length
+    #The while method will always return a value of nil
+      yield array[i]
+      i = i + 1
+    end #end yield
+     
+  else
+      puts "No block was given!"
+  end
+end   
+  my_each(list) {|i| puts #{i}}
 end
+  
